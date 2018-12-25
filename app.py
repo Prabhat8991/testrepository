aa8a6826f12cf04156aff8dfe509bc8ff410c0e1
prabhat.singh <Singh.binsu500>
2018-12-25 19:44:22 +0530

    changes

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ from resources.items import Item, ItemList
 import os
 
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #Turns off Flask tracker not SQLALchemy Tracker
 app.secret_key = 'jose'
+
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
