3bffce17ea052011da3547dfb9c32249b411c636
prabhat.singh <Singh.binsu500>
2018-12-25 19:47:21 +0530

    debugged

--- models/item.py
+++ models/item.py
@@ -5,8 +5,6 @@ class ItemModel(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision = 2))
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
-
     def __init__(self, name, price):
         self.name = name
         self.price = price
